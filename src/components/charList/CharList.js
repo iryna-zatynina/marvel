7289962e74ebae5c532346649c03174e4572b47c
iryna-zatynina <ira.zat1997@gmail.com>
2022-11-29 17:28:34 +0100

    add character info

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -24,14 +24,12 @@ class CharList extends Component{
             .then(this.onListLoaded)
             .catch(this.onError)
     }
-
     onListLoaded = (list) => {
         this.setState({
             list,
             loading: false
         })
     }
-
     onError = () => {
         this.setState({
             loading: false,
@@ -49,7 +47,8 @@ class CharList extends Component{
             return (
                 <li
                     className="char__item"
-                    key={item.id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
                 </li>
